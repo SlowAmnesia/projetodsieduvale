452d142799654a25709a21dbfa7be591f0d0db72
Tom <tomjaum@gmail.com>
2025-06-07 19:53:47 -0300

    responsividade e posts sendo gerados por json

--- script.js
+++ script.js
@@ -1,16 +1,72 @@
-const jsonData = `
-{
-    "nickname": "Teste", 
-    "user": "@slowamnesia", 
-    "time_passed": 14, 
-    "content_text": "Exemplo de texto escrito em uma publicação normal de rede social exemplo de texto escrito em uma publicação normal de rede social exemplo de texto escrito em uma publicação normal de rede social", 
-    "content_image": "./assets/cat.jpeg"
-}`;
-const objeto = JSON.parse(jsonData);
+document.addEventListener("DOMContentLoaded", program);
 
-document.querySelector(".nickname").innerHTML = objeto.user;
-document.querySelector(".user").innerHTML = objeto.user;
-document.querySelector(".time-passed").innerHTML = objeto.time_passed;
-document.querySelector(".content-text").innerHTML = objeto.content_text;
-document.querySelector(".content-image").src = objeto.content_image;
+async function program(){
+    const posts = await fetch('./data/posts.json')
+        .then(response => response.json())
+        .then(data => {return data})
+        .catch(error => console.log(error));
 
+    posts.forEach(post => criarPost(post));
+}
+
+function criarPost(objetoPost){
+    // criação dos elementos  
+    const post = document.createElement('div');
+    post.classList.add('post');
+
+    const profilePicture = document.createElement('div');
+    profilePicture.classList.add('profile-picture');
+
+    const img = document.createElement('img');
+    if (objetoPost.profile_picture){
+        img.src = objetoPost.profile_picture;
+    } else{
+        img.src = './assets/default_profile_picture.png';
+    }
+
+    const content = document.createElement('div');
+    content.classList.add('content');
+
+    const contentHeader = document.createElement('div');
+    contentHeader.classList.add('content-header');
+
+    const nickname = document.createElement('span');
+    nickname.classList.add('nickname');
+    nickname.innerHTML = objetoPost.nickname;
+
+    const user = document.createElement('span');
+    user.classList.add('user');
+    user.innerHTML = objetoPost.user;
+
+    const timePassed = document.createElement('span');
+    timePassed.classList.add('time-passed');
+    timePassed.innerHTML = objetoPost.time_passed
+    
+    const contentText = document.createElement('div');
+    contentText.classList.add('content-text');
+    contentText.innerHTML = objetoPost.content_text;
+
+    const contentPicture = document.createElement('div');
+    contentPicture.classList.add('content-picture');
+
+    const contentImage = document.createElement('img');
+    contentImage.classList.add('content-image');
+    contentImage.src = objetoPost.content_image;
+
+    //junção dos elementos
+    contentPicture.appendChild(contentImage);
+    contentHeader.appendChild(nickname);
+    contentHeader.appendChild(user);
+    contentHeader.appendChild(timePassed);
+
+    content.appendChild(contentHeader);
+    content.appendChild(contentText);
+    content.appendChild(contentPicture);
+
+    profilePicture.appendChild(img);
+
+    post.appendChild(profilePicture);
+    post.appendChild(content);
+
+    document.querySelector('.container').appendChild(post);
+}
