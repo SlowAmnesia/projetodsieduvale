f4419b8711dbd773a50ec113f2cba1c304bbadf6
Tom <tomjaum@gmail.com>
2025-06-07 16:25:19 -0300

    minor changes

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-const jsonData = '{"nickname": "Teste", "user": "@slowamnesia", "time_passed": 14, "content_text": "Exemplo de texto escrito em uma publicação normal de rede social exemplo de texto escrito em uma publicação normal de rede social exemplo de texto escrito em uma publicação normal de rede social", "content_image": "./assets/cat.jpeg"}';
+const jsonData = `
+{
+    "nickname": "Teste", 
+    "user": "@slowamnesia", 
+    "time_passed": 14, 
+    "content_text": "Exemplo de texto escrito em uma publicação normal de rede social exemplo de texto escrito em uma publicação normal de rede social exemplo de texto escrito em uma publicação normal de rede social", 
+    "content_image": "./assets/cat.jpeg"
+}`;
 const objeto = JSON.parse(jsonData);
 
 document.querySelector(".nickname").innerHTML = objeto.user;
